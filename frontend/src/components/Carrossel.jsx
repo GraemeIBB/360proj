518dd8425c4c8120eeebc0d9e27be879df40128b
Graeme B <vooner90@gmail.com>
2026-03-09 12:18:56 -0700

    feat: backend init + frontend carrossel

---
+++ frontend/src/components/Carrossel.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react"
+import medOutput from '../assets/med_output.jpg'
+
+function Carrossel(){
+    //
+    return(
+        <>
+        <div className="">
+        <img src={medOutput} alt="text image"/>
+        </div>
+        </>
+    )
+}
+
+export defautl Carrossel
