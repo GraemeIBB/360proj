34915b4ba23508492af0d49efe66e6609088d755
Jaxon Hay <jaxonmhay@gmail.com>
2026-03-22 16:14:09 -0700

    Added frontend for Book Posting page, need to implement backend still

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './Home.jsx'
 import Login from './Login.jsx'
+import PostBook from './PostBook.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/post-book" element={<PostBook />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
